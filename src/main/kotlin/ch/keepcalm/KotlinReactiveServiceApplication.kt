178f54f3c28f86a6a680d6938f55868396f45bca
Marcel Widmer <marzelwidmer@gmail.com>
2019-07-31 16:57:32 +0200

    Add RestController Class with Streaming Interface

--- src/main/kotlin/ch/keepcalm/KotlinReactiveServiceApplication.kt
+++ src/main/kotlin/ch/keepcalm/KotlinReactiveServiceApplication.kt
@@ -5,8 +5,13 @@ import org.springframework.boot.runApplication
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
 import reactor.core.publisher.toFlux
 import java.time.Duration
@@ -19,6 +24,23 @@ class KotlinReactiveServiceApplication
 
 fun main(args: Array<String>) {
     runApplication<KotlinReactiveServiceApplication>(*args)
+}
+
+@RestController
+@RequestMapping(value = ["/api/pizzas"])
+class PizzaController(private val service: PizzaService){
+
+    //  http :8080/api/pizzas
+    @GetMapping
+    fun all() = service.getAllPizzas()
+
+    //  http :8080/api/pizzas/f162dd0b-81af-4bbe-a4fc-a9e97181e201
+    @GetMapping(value = ["/{id}"])
+    fun byId(@PathVariable id: String) = service.getPizzaById(id = id)
+
+    // http -S  :8080/api/pizzas/f162dd0b-81af-4bbe-a4fc-a9e97181e201/orders
+    @GetMapping(value = ["/{id}/orders"], produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
+    fun orders(@PathVariable id: String) = service.getOrdersForPizzaId(pizzaId = id)
 }
 
 @Service
